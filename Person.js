e7a22756cc8788fbb6e02a043ec3fcf30aa7f7ff
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-08 21:34:07 +0900

    外部ファイル読み込み

---
+++ Person.js
@@ -0,0 +1,27 @@
+class Person {
+    name;
+    birth;
+
+    constructor(name, birth) {
+    this.name = name;
+    this.birth = birth;
+    }
+
+    getAge() {
+      const now = new Date();
+      let age = now.getFullYear() - this.birth.getFullYear();
+
+      //誕生日がまだの場合は年齢から1引く
+      const thisBirth = new Date(
+        now.getFullYear(),
+        this.birth.getMonth(),
+        this.birth.getDate()
+      );
+
+      if (now < thisBirth) {
+        age--;
+      } 
+
+      return age;
+    }
+  }
