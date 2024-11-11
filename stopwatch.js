a998603f22a53f775c73d5053d21620a3e0331ec
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-11 20:23:39 +0900

    関数実装

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,19 @@
+let addZero = (value) => {
+  if (value < 10) {
+    value = '0' + value;
+  }
+
+  return value;
+}
+
 let now = new Date();
 let seconds = now.getSeconds();
-if (seconds < 10) {
-  seconds = '0' + seconds;
-}
-document.getElementById('timer').innerHTML = seconds;
+seconds = addZero(seconds);
+
+let minutes = now.getMinutes();
+minutes = addZero(minutes);
+
+let hours = now.getHours();
+hours = addZero(hours);
+
+document.getElementById('timer').innerHTML = hours + ':' + minutes + ':' + seconds;
