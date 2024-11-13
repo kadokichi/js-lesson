ce76f95dd20a9255f533615b03abb77799b72c1a
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-13 20:47:58 +0900

    createElement,appendChild

--- ajax.js
+++ ajax.js
@@ -11,4 +11,24 @@ let images = [
   }
 ];
 
-alert(images[0].caption);
+//alert(images[0].caption);
+
+let img;
+let caption;
+let div;
+
+img = document.createElement('img');
+img.setAttribute('src', images[0].path);
+// <img src="img/img01.jpg">
+
+caption = document.createElement('div');
+caption.className = 'inner';
+caption.innerHTML = '<p>' + images[0].caption + '<span>' + images[0].name + '</span></p>';
+//  <div class="inner"><p>caption<span>name</span></p></div>
+
+div = document.createElement('div');
+div.className = 'photo';
+div.appendChild(img);
+div.appendChild(caption);
+
+document.getElementById('img_unit').appendChild(div);
