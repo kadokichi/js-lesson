2b56e610db9eee0e7a61438ea056acf05d9bbd3e
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-13 20:27:53 +0900

    JSONデータを配列で操作

---
+++ ajax.js
@@ -0,0 +1,14 @@
+let images = [
+  {
+    "path": "img/img01.jpg",
+    "name": "name01",
+    "caption": "コメント1"
+  },
+  {
+    "path": "img/img02.jpg",
+    "name": "name02",
+    "caption": "コメント2"
+  }
+];
+
+alert(images[0].caption);
