6b5b30e2e57a77137c85b48911dbb2a07a25e542
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-13 21:51:19 +0900

    ajax通信でデータを取得

--- ajax.js
+++ ajax.js
@@ -2,3 +2,11 @@ let ajax = new XMLHttpRequest();
 ajax.open('GET','https://h2o-space.com/htmlbook/images.php');
 ajax.responseType = 'json';
 ajax.send(null);
+
+ajax.onreadystatechange = function() {
+  if (ajax.readyState === XMLHttpRequest.DONE && ajax.status === 200) {
+    for (let i=0; i<this.response.length; i++) {
+      alert(this.response[i].path);
+    }
+  }
+}
